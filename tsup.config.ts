10440f7951d044d9db643656d76dd6b4bc38e04c
Omar Khatib <omar@omarkhatib.co>
2025-10-20 14:49:22 +0200

    Change output format in tsup configuration from ESM to CJS for compatibility with Node.js

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  format: ["esm"],
+  format: ["cjs"],
   target: "node22",
   outDir: "dist",
   clean: true,
